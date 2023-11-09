f4237b2ec436dc7ada2b64bd2e4c921dc07117c7
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-09 20:33:54 -0300

    configurando 'tasks_controller.rb' para mostrar notificações

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+     def index
+          @tasks = Task.all
+end
+
+before_action :authenticate_user!
+
+def new
+     @task = Task.new
+end
+
+def create
+     @task = Task.new(task_params)
+     if @task.save
+          flash[:success] = 'Tarefa criada com sucesso!'
+          redirect_to tasks_path
+     else
+          flash[:error] = 'Erro ao criar a tarefa.'
+          render 'new'
+     end
+end
+
+
+# Implementar as ações para editar, atualizar e excluir tarefas aqui
+
+private
+
+     def task_params
+          params.require(:task).permit(:title, :assignee, :due_date, :priority)
+     end
 end
