517bca89d031330e0eee7fabac8555745888746f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-10 19:25:24 -0300

    Atualizando o controlador para lidar com a ordenação (tasks_controller.rb), isso permite que o usuário clique nos links para ordenar a lista de tarefas por data de vencimento ou prioridade.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,34 @@
 class TasksController < ApplicationController
      def index
-          @tasks = Task.all
-end
-
-before_action :authenticate_user!
-
-def new
-     @task = Task.new
-end
-
-def create
-     @task = Task.new(task_params)
-     if @task.save
-          flash[:success] = 'Tarefa criada com sucesso!'
-          redirect_to tasks_path
-     else
-          flash[:error] = 'Erro ao criar a tarefa.'
-          render 'new'
+          @tasks = if params[:sort] == 'due_date'
+                         Task.order(due_date: :asc)
+                    elsif params[:sort] == 'priority'
+                         Task.order(priority: :desc)
+                    else
+                         Task.all
+                    end
      end
-end
 
+     before_action :authenticate_user!
 
-# Implementar as ações para editar, atualizar e excluir tarefas aqui
+     def new
+          @task = Task.new
+     end
 
-private
+     def create
+          @task = Task.new(task_params)
+          if @task.save
+               flash[:success] = 'Tarefa criada com sucesso!'
+               redirect_to tasks_path
+          else
+               flash[:error] = 'Erro ao criar a tarefa.'
+               render 'new'
+          end
+     end
+
+     #Implementar as ações para editar, atualizar e excluir tarefas aqui
+
+     private
 
      def task_params
           params.require(:task).permit(:title, :assignee, :due_date, :priority)
