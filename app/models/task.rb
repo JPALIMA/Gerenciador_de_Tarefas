46fc899d7561c641ddfe855222ad6d2e56f82643
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-14 23:34:47 -0300

    adicionar a capacidade de associar tarefas a categorias ou adicionar tags para facilitar a organização e a busca

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+     belongs_to :user
+     has_adn_belongs_to_many :categories
+     has_and_belongs_to_many :tags
+
+     validates :title, presence: true
+     validates :due_date, presence: true
+     validates :priority, presence: true
 end
