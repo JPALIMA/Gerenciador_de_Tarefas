3a9dd91e4a4c760ba84771f8e8c1db8aa524a48b
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-18 17:49:04 -0300

    erro de digitação. O método correto é has_and_belongs_to_many :categories, e não .has_adn_belongs_to_many

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
      belongs_to :user
-     has_adn_belongs_to_many :categories
+     has_and_belongs_to_many :categories
      has_and_belongs_to_many :tags
 
      validates :title, presence: true
