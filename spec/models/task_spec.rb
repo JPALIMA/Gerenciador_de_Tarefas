905bab05037d7f157fc07e4549b0fe789702900c
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-17 22:59:18 -0300

    criando um arquivo de teste na pasta .E adicionando alguns testes básicos.spec/models/task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "é válido com atributos válidos" do
+    task = Task.new(
+      title: "Exemple Task",
+      assignee: "John Doe",
+      due_date: Date.today + 7,
+      priority: "High"
+    )
+    expect(task).to be_valid
+  end
+
+  it "não é válido sem título" do
+    task = Task.new(title: nil)
+    expect(task).to_not be_valid
+  end
+
+  it "não é válido sem data de vencimento" do
+    task = Task.new(due_date: nil)
+    expect(task).to_not be_valid
+  end
+
+  it "não é válido sem prioridade" do
+    task = Task.new(priority: nil)
+    expect(task).to_not be_valid
+  end
 end
