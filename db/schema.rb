39e990585e25e24072769aaac7dcb76eba7bf735
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-18 17:41:39 -0300

    O arquivo 'db:schema:load é gerado automaticamente a partir do estado atual do banco de dados. Este arquivo é a fonte que o Rails usa para definir esquema ao executar in/rails'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_010541) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_18_012754) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "assignee"
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_010541) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
