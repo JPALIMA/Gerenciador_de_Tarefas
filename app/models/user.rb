8d6281de3a15cad23c1a969b2f6a111a7b7ad6e6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-21 20:35:48 -0300

    modelo User não tem um atributo chamado 'name', alterando para 'username'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks
 
-  validates :name, presence: true
+  validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
