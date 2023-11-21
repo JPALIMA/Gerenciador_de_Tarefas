be949d61a245365e0ba9942aeb2ff069b901c292
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-20 22:27:50 -0300

    o nome 'User' já está sendo usado em seu aplicativo ou é reservado pelo Ruby on Rails, substituindo 'User' pelo nome escolhido 'Usuario'

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let(:user) { User.create(username: 'joao', email: 'joao@examplo.com', password: 'senha') }
+  let(:user) { User.create(name: 'joao', email: 'joao@exemplo.com', password: 'senha')}
 
   it 'é válido com atributos válidos' do
     task = Task.new(
-      title: 'Exemple Task',
+      title: 'Exemplo Task',
       assignee: 'joao',
-      due_date: '25-11-2023', # Certifique-se de ajustar a data conforme necessário
+      due_date: '25-11-2023',
       priority: 'alta',
-      user: user
+      user: user #Associando a tarefa ao usuário
     )
+
     expect(task).to be_valid
+  end
+
+  it "não é válido sem título" do
+    task = Task.new(title: nil)
+    expect(task).to_not be_valid
   end
 
   it "não é válido sem data de vencimento" do
@@ -19,7 +25,7 @@ RSpec.describe Task, type: :model do
     expect(task).to_not be_valid
   end
 
-  it "não é válido sem prioridade" do
+  it "não é valido sem prioridade" do
     task = Task.new(priority: nil)
     expect(task).to_not be_valid
   end
