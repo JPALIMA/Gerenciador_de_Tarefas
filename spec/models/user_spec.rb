ca9c73b707afc698d24ef2dd008731e9789331cf
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-11-24 23:01:19 -0300

    atualizando o arquivo user_spec.rb adicionando alguns exemplos válidos ou removendo a pendência.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-     pending "adcione alguns exemplos a (ou exclua) #{__FILE__}"
-end
+  it 'é válido com atributos válidos' do
+    user = User.new(
+      username: 'joao',
+      email: 'joao@exemplo.com',
+      password: 'senha'
+    )
+    expect(user).to be_valid
+  end
 
+  it 'não é válido sem um e-mail' do
+    user = User.new(username: 'joao', password: 'senha')
+    expect(user).to_not be_valid
+  end
+
+  # Adicione mais exemplos conforme necessário
+
+  # Remova a pendência
+  pending "adcione alguns exemplos a (ou exclua) #{__FILE__}"
+end
